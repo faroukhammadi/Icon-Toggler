509e7de1772980b58364df8c111b0213151ecd7b
Farouk Hammadi <96744392+faroukhammadi@users.noreply.github.com>
2026-05-07 20:49:02 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll(".favorite-icon");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+
+    if (btn.classList.contains("filled")) {
+
+      btn.classList.remove("filled");
+      btn.innerHTML = "&#9825;";
+
+    } else {
+
+      btn.classList.add("filled");
+      btn.innerHTML = "&#10084;";
+
+    }
+
+  });
+});
